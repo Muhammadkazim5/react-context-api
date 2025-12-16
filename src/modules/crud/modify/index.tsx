5f7ba4664e126162ac14cfd636fb93ac5d290f7f
muhammadkazim5 <muhammadkazim5806@gmail.com>
2025-12-16 15:54:04 +0500

    update

---
+++ src/modules/crud/modify/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModifyCrud = () => {
+  return (
+    <div>ModifyCrud</div>
+  )
+}
+
+export default ModifyCrud
---
+++ src/modules/crud/view/index.tsx
@@ -0,0 +1,7 @@
+const ViewCrud = () => {
+  return (
+    <div>ViewCrud</div>
+  )
+}
+
+export default ViewCrud
